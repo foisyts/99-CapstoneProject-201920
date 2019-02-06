5d91ab344700b2fd8ad9ef344afbd056569694a3
hedricsd <hedricsd@rose-hulman.edu>
2019-02-06 14:36:03 -0500

    Calibrating Arm

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,13 +17,16 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
     run_test_arm()
+    run_test_calibrate_arm()
 
 
 def run_test_arm():
     robot = rosebot.RoseBot()
     robot.arm_and_claw.raise_arm()
 
-
+def run_test_calibrate_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.calibrate_arm()
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
