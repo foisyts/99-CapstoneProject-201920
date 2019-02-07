c580f6f40cdda03b04fac7d42a5b0217431aac1e
hoeventl <hoeventl@rose-hulman.edu>
2019-02-07 14:52:44 -0500

    corrected move_arm_to_position in rosebot.py

--- src/rosebot.py
+++ src/rosebot.py
@@ -254,7 +254,7 @@ class ArmAndClaw(object):
             while True:
                 self.motor.turn_on(-100)
                 changing_pos = self.motor.get_position()
-                if changing_pos >= desired_arm_position:
+                if changing_pos <= desired_arm_position:
                     self.motor.turn_off()
                     break
         elif first_pos < desired_arm_position:
