1bf898ab5ade20c1e26f13918b5ec6a309f744ad
hoeventl <hoeventl@rose-hulman.edu>
2019-02-10 21:49:09 -0500

    sandbox stuff for lambda of soundsystem

--- sandbox/sandbox2.py
+++ sandbox/sandbox2.py
@@ -1,2 +1,7 @@
 # Put whatever you want in this module and do whatever you want with it.
 # It exists here as a place where you can "try out" things without harm.
+
+# Set the Button callbacks:
+beep_button["command"] = lambda: handle_beep(mqtt_sender, number_entry, frequency_entry)
+tone_button["command"] = lambda: handle_tone(mqtt_sender, duration_entry, frequency_entry)
+phrase_button["command"] = lambda: handle_phrase(mqtt_sender, phrase_entry)
