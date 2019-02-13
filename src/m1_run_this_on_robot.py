b0ef1448453825acd3dbff58e3634c4d7b9c8541
foisyts <foisyts@rose-hulman.edu>
2019-02-13 15:05:10 -0500

    camera display - no values
    and
    
    set up m1_run on robot

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -8,7 +8,7 @@
 import rosebot
 import mqtt_remote_method_calls as com
 import time
-
+import shared_gui_delegate_on_robot
 
 def main():
     """
@@ -16,16 +16,25 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    camera_tester()
 
 
 def real_thing():
     robot = rosebot.RoseBot()
-    delegate_that_receives = DelegateThatReceives(robot)
+    delegate_that_receives = shared_gui_delegate_on_robot.DelegateThatReceives(robot)
     mqtt_receiver = com.MqttClient()
     mqtt_receiver.connect_to_pc()
 
     while True:
         time.sleep(0.01)
+        if delegate_that_receives.is_time_to_stop:
+            break
+
+
+def camera_tester():
+    robot = rosebot.RoseBot()
+    robot.drive_system.display_camera_data()
+
 
 
 # -----------------------------------------------------------------------------
