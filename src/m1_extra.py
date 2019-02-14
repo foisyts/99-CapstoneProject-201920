31b5f08650e75a0e0c9452ccdf594d97b10059ad
foisyts <foisyts@rose-hulman.edu>
2019-02-14 14:17:23 -0500

    tweaked sleep

--- src/m1_extra.py
+++ src/m1_extra.py
@@ -5,13 +5,13 @@ import m3_extra as m3
 
 def drive_and_beep_with_ir(initial, rate_of_increase):
     robot = rosebot.RoseBot()
-    robot.arm_and_claw.calibrate_arm()
+    # robot.arm_and_claw.calibrate_arm()
     robot.drive_system.go(50, 50)
     while True:
         distance = robot.sensor_system.ir_proximity_sensor.get_distance_in_inches()
         robot.sound_system.beep()
         sleep = m3.get_sleep(initial, rate_of_increase)
-        time.sleep(sleep)
+        time.sleep(sleep / 10)
         if distance <= 2:
             break
     robot.drive_system.stop()
