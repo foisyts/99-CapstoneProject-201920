22c63b68347dee389a0c3511aa548a792785dadf
hedricsd <hedricsd@rose-hulman.edu>
2019-02-13 22:52:59 -0500

    feature 9 completed

--- src/m3_run_this_on_laptop.py
+++ src/m3_run_this_on_laptop.py
@@ -12,6 +12,7 @@ import tkinter
 from tkinter import ttk
 import shared_gui
 
+
 def main():
     """
     This code, which must run on a LAPTOP:
@@ -24,13 +25,11 @@ def main():
     mqtt_sender = com.MqttClient()
     mqtt_sender.connect_to_ev3()
 
-
     # -------------------------------------------------------------------------
     # The root TK object for the GUI:
     # -------------------------------------------------------------------------
     root = tkinter.Tk()
     root.title("CSSE 120 Capstone Project, Winter 2018-19")
-
 
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
@@ -41,9 +40,8 @@ def main():
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
-    teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame = get_shared_frames(main_frame,
+    teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, led_frame = get_shared_frames(main_frame,
                                                                                                        mqtt_sender)
-
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
@@ -53,14 +51,12 @@ def main():
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
-    grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame)
-
+    grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, led_frame)
 
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
     root.mainloop()
-
 
 
 def get_shared_frames(main_frame, mqtt_sender):
@@ -69,17 +65,51 @@ def get_shared_frames(main_frame, mqtt_sender):
     control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
     drive_system_frame = shared_gui.get_drive_system_frame(main_frame, mqtt_sender)
     sound_system_frame = shared_gui.get_sound_system_frame(main_frame, mqtt_sender)
+    led_frame = get_sams_frame(main_frame, mqtt_sender)
 
-    return teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame
+    return teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, led_frame
 
 
-def grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame):
+def grid_frames(teleop_frame, arm_frame, control_frame, drive_system_frame, sound_system_frame, led_frame):
     teleop_frame.grid(row=0, column=0)
     arm_frame.grid(row=1, column=0)
     control_frame.grid(row=2, column=0)
     drive_system_frame.grid(row=0, column=1)
     sound_system_frame.grid(row=1, column=1)
+    led_frame.grid(row=0, column=3)
 
+
+def get_sams_frame(window, mqtt_sender):
+    # Construct the frame to return:
+    frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
+    frame.grid()
+
+    # Construct the widgets on the frame:
+    frame_label = ttk.Label(frame, text="Sam's Frame", font='Arial 14 bold')
+    led_picker_upper_button = ttk.Button(frame, text="Pick it up")
+    initial_label = ttk.Label(frame, text='Initial')
+    initial_entry = ttk.Entry(frame, width=8)
+    rate_label = ttk.Label(frame, text='Rate of increase')
+    rate_entry = ttk.Entry(frame, width=8)
+
+    # Grid the widgets:
+    frame_label.grid(row=0, column=1)
+    led_picker_upper_button.grid(row=3, column=1)
+    initial_label.grid(row=1, column=0)
+    initial_entry.grid(row=2, column=0)
+    rate_label.grid(row=1, column=2)
+    rate_entry.grid(row=2, column=2)
+
+    # Set the Button callbacks:
+    led_picker_upper_button["command"] = lambda: handle_led_picker_upper(mqtt_sender, initial_entry,
+                                                                         rate_entry)
+
+    return frame
+
+
+def handle_led_picker_upper(mqtt_sender, initial_entry, rate_entry):
+    print("Picking up with LED.")
+    mqtt_sender.send_message("led_picker_upper", [initial_entry.get(), rate_entry.get()])
 
 
 # -----------------------------------------------------------------------------
